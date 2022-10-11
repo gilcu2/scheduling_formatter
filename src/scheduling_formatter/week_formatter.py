9927eade495906670e14a178471aed39879d76fe
Reynaldo Gil <gilcu2@gmail.com>
2022-10-11 21:21:58 +0200

    Simple test week working

--- src/scheduling_formatter/week_formatter.py
+++ src/scheduling_formatter/week_formatter.py
@@ -27,20 +27,11 @@ Week_Scheduling = Dict[WeekDays, Day_Scheduling]
 opening_line = "A restaurant is open:\n"
 
 
-def check_week(scheduling: Week_Scheduling) -> Result[None, str]:
-    for day in WeekDays:
-        check_day_result = check_day(scheduling.get(day), scheduling.get(next_day[day]),
-                                     scheduling.get(previous_day[day]))
-        if check_day_result.is_err:
-            return check_day_result
-    return Ok(None)
-
-
 def format_from_formatted_days(formatted_days: Dict[WeekDays, str]) -> str:
     accumulator = opening_line
     for day in WeekDays:
         if day in formatted_days:
-            accumulator += f"{formatted_days[day]}\n"
+            accumulator += f"{day.capitalize()}: {formatted_days[day]}\n"
         else:
             accumulator += f"{day.capitalize()}: {closed_phrase}\n"
 
@@ -48,14 +39,12 @@ def format_from_formatted_days(formatted_days: Dict[WeekDays, str]) -> str:
 
 
 def format_week(scheduling: Week_Scheduling) -> Result[str, str]:
-    check_result = check_week(scheduling)
-
-    if check_result.is_err:
-        Err(check_result.unwrap_err())
-
-    formatted_days = {
-        day: format_day(scheduling.get(day), scheduling.get(next_day[day]), scheduling.get(previous_day[day]))
-        for day in WeekDays
-    }
+    formatted_days: Dict[WeekDays, str] = {}
+    for day in WeekDays:
+        possible_formatted = format_day(scheduling.get(day), scheduling.get(next_day[day]),
+                                        scheduling.get(previous_day[day]))
+        if possible_formatted.is_err:
+            return possible_formatted
+        formatted_days[day] = possible_formatted.unwrap()
 
     return Ok(format_from_formatted_days(formatted_days))
--- tests/scheduling_formatter/week_formatter_test.py
+++ tests/scheduling_formatter/week_formatter_test.py
@@ -6,7 +6,7 @@ from utils.str_extensions import clean
 
 def test_format_from_formatted_days():
     # Given formatted days
-    formatted_days = {WeekDays.monday: "Monday: 9 AM - 8 PM"}
+    formatted_days = {WeekDays.monday: "9 AM - 8 PM"}
 
     # And the expected formatted
     expected = """
@@ -32,13 +32,13 @@ def test_format_week():
     # Given a scheduling
     scheduling = {
         WeekDays.monday: [
-            Action(type=ActionType.open, value=32400),
-            Action(type=ActionType.open, value=32400),
+            Action(type=ActionType.open, value=9 * 3600),
+            Action(type=ActionType.close, value=20 * 3600),
         ],
     }
 
     # And the expected result
-    expected = format_from_formatted_days({WeekDays.monday: "Monday: 9 AM - 8 PM"})
+    expected = format_from_formatted_days({WeekDays.monday: "9 AM - 8 PM"})
 
     # When format it
     r = format_week(scheduling).unwrap()
